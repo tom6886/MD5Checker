43c656f8a46e9ece778b373bfe3da1d38a8013e4
汤波 <478185912@qq.com>
2017-12-14 15:54:51 +0800

    init
    初始化

---
+++ MD5Reader/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace MD5Reader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.memoEdit1 = new DevExpress.XtraEditors.MemoEdit();
+            this.btn_file = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.memoEdit1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // memoEdit1
+            // 
+            this.memoEdit1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.memoEdit1.Location = new System.Drawing.Point(0, 0);
+            this.memoEdit1.Name = "memoEdit1";
+            this.memoEdit1.Size = new System.Drawing.Size(540, 302);
+            this.memoEdit1.TabIndex = 0;
+            // 
+            // btn_file
+            // 
+            this.btn_file.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btn_file.Location = new System.Drawing.Point(0, 302);
+            this.btn_file.Name = "btn_file";
+            this.btn_file.Size = new System.Drawing.Size(540, 37);
+            this.btn_file.TabIndex = 1;
+            this.btn_file.Text = "选择文件";
+            this.btn_file.UseVisualStyleBackColor = true;
+            this.btn_file.Click += new System.EventHandler(this.btn_file_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 339);
+            this.Controls.Add(this.memoEdit1);
+            this.Controls.Add(this.btn_file);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "读取文件MD5值";
+            ((System.ComponentModel.ISupportInitialize)(this.memoEdit1.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.MemoEdit memoEdit1;
+        private System.Windows.Forms.Button btn_file;
+    }
+}
+
---
+++ MD5Reader/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MD5Reader
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        private double progress;
+
+        private DateTime beginTime;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            memoEdit1.ReadOnly = true;
+        }
+
+        private void btn_file_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = @"C:\";
+            //ofd.Filter = "Image Files(*.JPG;*.PNG;*.jpeg;*.GIF;*.BMP)|*.JPG;*.PNG;*.GIF;*.BMP;*.jpeg|All files(*.*)|*.*";
+            ofd.RestoreDirectory = true;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                if (!File.Exists(ofd.FileName)) { memoEdit1.Text = "未找到指定文件！"; return; }
+
+                FileInfo file = new FileInfo(ofd.FileName);
+
+                double length = Math.Ceiling(file.Length / 1024.0);
+
+                if (length < 102400 * 10)
+                {
+                    beginTime = DateTime.Now;
+                    string md5Str = MD5Checker.Check(ofd.FileName);
+                    memoEdit1.Text = "验算完成，MD5值：" + md5Str + "\r\n";
+                    memoEdit1.MaskBox.AppendText("验算耗费时间：" + (DateTime.Now - beginTime).TotalSeconds + "s");
+                }
+                else
+                {
+                    memoEdit1.Text = "文件大小超过1G，开启异步计算\r\n";
+                    progress = 0;
+                    beginTime = DateTime.Now;
+                    MD5Checker checker = new MD5Checker();
+                    checker.AsyncCheckProgress += Checker_AsyncCheckProgress;
+                    checker.AsyncCheck(ofd.FileName);
+                }
+            }
+        }
+
+        private void Checker_AsyncCheckProgress(AsyncCheckEventArgs e)
+        {
+            if (e.State == AsyncCheckState.Checking)
+            {
+                double pro = Convert.ToDouble(e.Value);
+                if (pro - progress < 1) { return; }
+                progress = pro;
+                SetEdit("验算进度：" + pro + "%\r\n");
+            }
+            else
+            {
+                SetEdit("验算完成，MD5值：" + e.Value + "\r\n");
+                SetEdit("验算耗费时间：" + (DateTime.Now - beginTime).TotalSeconds + "s");
+            }
+        }
+
+        private delegate void setEdit(string text);
+
+        private void SetEdit(string text)
+        {
+            if (memoEdit1.InvokeRequired)
+            {
+                BeginInvoke(new setEdit(SetEdit), text);
+            }
+            else
+            {
+                memoEdit1.MaskBox.AppendText(text);
+            }
+        }
+    }
+}
